3c342ecda0735de57229f09fd943566de12ebabd
LoJoSho <LoJoShogaming@gmail.com>
2024-02-25 10:22:09 -0600

    feat: finish chapter 3 work

--- Chapters/Chapter 3/Chapter3.R
+++ Chapters/Chapter 3/Chapter3.R
@@ -75,3 +75,83 @@ dim(die) <- c(1, 2, 3)
 die
 
 # Matrices
+m <- matrix(die, nrow = 2)
+m
+# Fills it uo by going sideways instead
+m <- matrix(die, nrow = 2, byrow = TRUE)
+m
+
+# Arrays
+ar <- array(c(11:14, 21:24, 31:34), dim = c(2, 2, 3))
+ar
+
+hand1 <- c("ace", "king", "queen", "jack", "ten", "spades", "spades",
+            "spades", "spades", "spades")
+matrix(hand1, nrow = 5, byrow = TRUE)
+matrix(hand1, ncol = 2, byrow = TRUE)
+dim(hand1 <- c(5, 2))
+
+# classes
+dim(die) <- c(2, 3)
+typeof(die)
+class(die)
+
+# Dates and Times
+now <- Sys.time()
+now
+
+typeof(now)
+class(now)
+unclass(now)
+
+mil <- 1000000
+mil
+class(mil) <- c("POSIXct", "POSIXt")
+mil
+
+# Factors
+gender <- factor(c("male", "female", "female", "male"))
+typeof(gender)
+attributes(gender)
+unclass(gender)
+gender
+
+as.character(gender)
+
+card <- c("ace", "hearts", 1)
+card
+
+# Coercion
+sum(c(TRUE, TRUE, FALSE, FALSE))
+as.character(1)
+as.logical(1)
+as.numeric(TRUE)
+
+# Lists
+list1 <- list(100:130, "R", list(TRUE, FALSE))
+list1
+
+card <- list("ace", "Heartss", 1)
+card
+
+# Data Frames
+df <- data.frame(face = c("ace", "two", "six"), 
+                 suit = c("clubs", "clubs", "clubs"), value = c(1, 2, 3))
+df
+
+typeof(df)
+class(df)
+str(df)
+
+df <- data.frame(face = c("ace", "two", "six"),
+                 suit = c("clubs", "clubs", "clubs"), value = c(1, 2, 3),
+                 stringsAsFactors = FALSE)
+
+head(deck)
+
+getwd()
+setwd('C:/Users/stick/Documents/GitHub/Data332/Chapters/Chapter 3/data')
+
+write.csv(deck, file = "cards1.csv", row.names = FALSE)
+
+
