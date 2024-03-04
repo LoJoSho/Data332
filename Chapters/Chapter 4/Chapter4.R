528e0cc9868f5e62090c4ebe67595b9e2f3e1fa9
LoJoSho <LoJoShogaming@gmail.com>
2024-03-04 10:54:32 -0600

    feat: finalize chapter 4 work

--- Chapters/Chapter 4/Chapter4.R
+++ Chapters/Chapter 4/Chapter4.R
@@ -14,7 +14,7 @@ vec[1:3]
 deck[-(2:52), 1:3]
 
 # Errors out
-deck[c(-1, 1), 1]
+#deck[c(-1, 1), 1]
 
 # Zero
 deck[0, 0]
@@ -29,6 +29,62 @@ deck[1, c(TRUE, TRUE, FALSE)]
 deck[1, c("face", "suit", "value")]
 
 # Deal a card
+deal <- function(cards) {
+  cards[1, ]
+}
+
+deal(deck)
+deal(deck)
+deal(deck)
+
+# Shuffle Cards
+
+deck2 <- deck[1:52, ]
+head(deck2)
+
+deck3 <- deck[c(2, 1, 3:52), ]
+head(deck3)
+
+## Randomizes the values
+random <- sample(1:52, size = 52)
+random
+
+deck4 <- deck[random, ]
+head(deck4)
+
+shuffle <- function(cards) {
+  random <- sample(1:52, size = 52)
+  cards[random, ]
+}
+
+deal(deck)
+deck2 <- shuffle(deck)
+deal(deck2)
+
+# Dollar signs and Double Brackets
+deck$value # Returns all values as a vector
+mean(deck$value)
+median(deck$value)
+
+lst <- list(numbers = c(1, 2), logical = T, strings = c("a", "b", "c"))
+lst
+
+lst[1] # Get the first value in the list (subset it)
+
+# sum(lst[1]) # errors out
+lst$numbers # returns the numbers
+sum(lst$numbers)
+lst[[1]] # Does the same thing as the $ notation, returns 1 2
+
+lst["numbers"] # get the element of the list
+lst[["numbers"]] # Same thing as above
+
+
+
+
+
+
+
 
 
 
